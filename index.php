5251c797d3e23964b648eeb1e3eed1143eac1d8c
Samuel <kiriyama.hikari@outlook.com>
2022-12-08 16:32:49 -0500

    Detalles

--- index.php
+++ index.php
@@ -50,11 +50,11 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
   <!-- Navbar on small screens -->
   <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
     <a href="equipos/Equipos.html" class="w3-bar-item w3-button w3-padding-large">Equipos</a>
-    <a href="./partidos/mostrarPartidos.php" class="w3-bar-item w3-button w3-padding-large">Juegos</a>
+    <a href="partidos/mostrarPartidos.php" class="w3-bar-item w3-button w3-padding-large">Juegos</a>
     <a href="grupos/elegirGrupo.php" class="w3-bar-item w3-button w3-padding-large">Posiciones</a>
     <a href="clasificatoria/clasificatoria.php" class="w3-bar-item w3-button w3-padding-large">Clasificatorias</a>
-    <a href="./favorito/mostrarFav.php" class="w3-bar-item w3-button w3-padding-large">Favoritos</a>
-    <a href="inicio_sesion/login.php" class="w3-bar-item w3-button w3-padding-large">Login</a>
+    <a href="favorito/mostrarFav.php" class="w3-bar-item w3-button w3-padding-large">Favoritos</a>
+    <a href="sesion/login.php" class="w3-bar-item w3-button w3-padding-large">Login</a>
   </div>
 </div>
 
@@ -69,8 +69,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-twothird">
-      <h1>Lorem Ipsum</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+      <h1>Copa Mundial de la FIFA Catar 2022</h1>
+      <h5 class="w3-padding-32">A través de este sitio web podrá ver diferentes detalles del Mundial de la FIFA Catar 2022</h5>
 
       <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
